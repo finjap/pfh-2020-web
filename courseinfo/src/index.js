8a63abd54cb475ac678818d669bdffa713930240
Finja Plaumann <finjaplaumann@Finjas-Air.fritz.box>
2020-11-10 13:52:16 +0100

    Homework 1_8

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -1,77 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-    console.log(props)
-    return (
-        <div>
-            <h1> {props.course} </h1>
-        </div>
-    )
-}
-const Content = (props) => {
-    console.log(props)
-    return(
-        <div>
-                <Part part={props.parts[0]}></Part>
-                <Part part={props.parts[1]}></Part>
-                <Part part={props.parts[2]}></Part>
-        </div>
-    )
-}
-const Part = (props) => {
-    return(
-        <div>
-            <p>
-                {props.part.name} {props.part.exercises}
-            </p>
-        </div>
-    )
-}
-const Total = (props) => {
-    console.log(props)
-    return(
-        <div>
-            <p>
-            Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-            </p>
-        </div>
-    )
-}
+
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7
-          },
-          {
-            name: 'State of a component',
-            exercises: 14
-          }
-        ]
-      }
+
+// save clicks of each button to its own state
+
+const [good, setGood] = useState(0)
+
+const [neutral, setNeutral] = useState(0)
+
+const [bad, setBad] = useState(0)
 
 
-    return (
-        <div>
-            <Header
-                course={course.name}>
-            </Header>
-            <Content
-                parts={course.parts}>
-            </Content>
-            <Total
-                parts={course.parts}>
-            </Total>
-        </div>
-    )
+
+return (
+
+<div>
+
+<div>Good: {good}</div>
+
+<div>Neutral: {neutral}</div>
+
+<div>Bad: {bad}</div>
+
+<div>Average: {(good + neutral)/2}</div>
+
+<button onClick={() => setGood(good + 1)}>   Good  </button>
+
+<button onClick={() => setNeutral(neutral + 1)}>  Neutral </button>
+
+<button onClick={() => setBad(bad + 1)}>  Bad </button>
+
+</div>
+
+)
+
 }
+
+
+
+ReactDOM.render(<App />,
+
+document.getElementById('root')
+
+)
 
 ReactDOM.render(<App/>, document.getElementById('root'))
