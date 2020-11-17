f44edc2278e400ac0d04ce9ac0f0fd226f0f24bc
Finja Plaumann <finjaplaumann@Finjas-Air.fritz.box>
2020-11-17 20:48:15 +0100

    phonebook 2_8

--- phonebook/src/index.js
+++ phonebook/src/index.js
@@ -4,17 +4,18 @@ import Person from './component/Person'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '1234567890' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
 
   const addName = (submitEvent) => {
        submitEvent.preventDefault()
-    setPersons([
-      ...persons,
-      {name: newName}
-    ])
+       setPersons([...persons, { name: newName, number: newNumber}])
+      
+   
   }
+
 
   return (
     <div>
@@ -22,13 +23,14 @@ const App = () => {
       <form onSubmit={addName}>
         <div>
           name: < input value={newName} onInput={inputEvent => setNewName(inputEvent.target.value) }/>
+          number: < input value={newNumber} onInput={inputEvent => setNewNumber(inputEvent.target.value) }/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-         {persons.map(listItem => <Person name={listItem.name}></Person>)}
+         {persons.map(listItem => <Person name={listItem.name} number={listItem.number}></Person>)}
     </div>
   )
 }
