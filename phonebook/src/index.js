e5da4c595a869c3d79d6eeed6db669d96a17fb3b
Finja Plaumann <finjaplaumann@Finjas-Air.fritz.box>
2020-11-18 09:29:44 +0100

    phonebook 2_11

--- phonebook/src/index.js
+++ phonebook/src/index.js
@@ -1,40 +1,59 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom';
 import Person from './component/Person'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '1234567890' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber] = useState('')
-
-  const addName = (submitEvent) => {
-       submitEvent.preventDefault()
-       setPersons([...persons, { name: newName, number: newNumber}])
-      
-   
+  const [persons, setPersons] = useState([
+    //   { name: 'Arto Hellas' , number: 8646354738},
+    //   { name: 'Peter Pan' , number: 45678909876543 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons').then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  const addName = (inputEvent) => {
+    inputEvent.preventDefault()
+    
+    axios
+    .post('http://localhost:3001/persons', { name: newName, number: newNumber, })
+    .then(response => {
+      setPersons(persons.concat(response.data))
+      setNewName('')
+      setNewNumber('')
+    })
   }
-
-
-  return (
+return (
+  <div>
+    <h2>Phonebook</h2>
+    <form onSubmit={addName}>
+      <div>
+        name: < input value={newName} onInput={inputEvent => setNewName(inputEvent.target.value)} />
+          number: <input value={newNumber} onInput={inputEvent => setNewNumber(inputEvent.target.value)} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+    <h2>Numbers</h2>
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: < input value={newName} onInput={inputEvent => setNewName(inputEvent.target.value) }/>
-          number: < input value={newNumber} onInput={inputEvent => setNewNumber(inputEvent.target.value) }/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-         {persons.map(listItem => <Person name={listItem.name} number={listItem.number}></Person>)}
+      {persons.map(listItem => <Person name={listItem.name} number={listItem.number}></Person>)}
     </div>
-  )
+  </div>
+)
 }
-
+axios.get('http://localhost:3001/persons')
+  .then(result => ReactDOM.render(
+    <App persons={result.data} />,
+    document.getElementById('root')
+  ))
 ReactDOM.render(
   <App />, document.getElementById('root')
 )
