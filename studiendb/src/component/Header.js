6ed6552a593a754592872db1d9b15a6fc43335fc
Finja Plaumann <finjaplaumann@Finjas-MacBook-Air.local>
2020-12-03 15:12:49 +0100

    ...

---
+++ studiendb/src/component/Header.js
@@ -0,0 +1,4 @@
+<div class="header" style=" height:200px;">
+  <h1>Header</h1>
+  <p>My supercool header</p>
+</div>
---
+++ studiendb/src/component/Studie.js
@@ -0,0 +1,9 @@
+// import React from 'react'
+const Studie = ({ titel, studienleiter, details, zeitraum, stunden, kriterien }) => {
+    return (
+      <div>
+          {titel} {studienleiter} {details} {zeitraum} {stunden} {kriterien}
+      </div>
+    )
+  }
+  export default Studie
